ca440a3482a433f5e32b9e46b7d40875ec5de081
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-14 13:26:42 -0700

    WIP: display contacts in index.html.
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -47,6 +47,16 @@ Contact.prototype.update = function() {
 
 let addressBook = new AddressBook();
 
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -55,6 +65,6 @@ $(document).ready(function() {
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   });
 });
